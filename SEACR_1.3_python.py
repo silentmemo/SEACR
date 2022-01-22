f58b9ac962857fd4c1d7be6d568a7b6d972a307a
silentmemo <38174655+silentmemo@users.noreply.github.com>
2022-01-22 19:33:01 +0800

    Create SEACR_1.3_python.py

---
+++ SEACR_1.3_python.py
@@ -0,0 +1,9 @@
+#### This script is based on the SEACR_1.3.sh. It is written in python form for my readability. This version is a lot more memory hungry, and thus should not be used for production. For Education use only. 
+
+import sys
+def make_signal_block(bedgraph_list,filename):
+    print("Generating bedgraph file from: ", filename)
+def main():
+    print("Running python version of SEACR_1.3.sh")
+
+main()
